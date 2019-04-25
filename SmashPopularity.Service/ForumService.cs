a70df07151144e0dad487f7d0330b963a79a2e38
M-D-Elliott <40653200+M-D-Elliott@users.noreply.github.com>
2019-04-25 03:15:39 -0500

    Added service layer

---
+++ SmashPopularity.Service/ForumService.cs
@@ -0,0 +1,53 @@
+﻿using SmashPopularity.Data;
+using SmashPopularity.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SmashPopularity.Service
+{
+    public class ForumService : IForum
+    {
+        private readonly ApplicationDbContext _context;
+        public ForumService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task Create(Forum forum)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(int forumId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Forum> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ApplicationUser> GetAllActiveUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Forum GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateForumDescription(int forumId, string newDescription)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateForumTitle(int forumId, string newTitle)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
