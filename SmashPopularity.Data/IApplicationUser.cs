0b84f43ab149512c595963702ff2d76c73f7ee70
M-D-Elliott <40653200+M-D-Elliott@users.noreply.github.com>
2019-05-02 15:27:10 -0500

    Created Profile Controller, AppUser and Services

---
+++ SmashPopularity.Data/IApplicationUser.cs
@@ -0,0 +1,17 @@
+﻿using SmashPopularity.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmashPopularity.Data
+{
+    public interface IApplicationUser
+    {
+        ApplicationUser GetByID(string id);
+        IEnumerable<ApplicationUser> GetAll();
+
+        Task SetProfileImage(string id, Uri uri);
+        Task IncrementRating(string id, Type type);
+    }
+}
---
+++ SmashPopularity.Service/ApplicationUserService.cs
@@ -0,0 +1,42 @@
+﻿using SmashPopularity.Data;
+using SmashPopularity.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmashPopularity.Service
+{
+    class ApplicationUserService : IApplicationUser
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ApplicationUserService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<ApplicationUser> GetAll()
+        {
+            return _context.ApplicationUsers;
+        }
+
+        public ApplicationUser GetByID(string id)
+        {
+            return GetAll().FirstOrDefault(u => u.Id == id);
+        }
+
+        public Task IncrementRating(string id, Type type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task SetProfileImage(string id, Uri uri)
+        {
+            var user = GetByID(id);
+            user.ProfileImageUrl = uri.AbsoluteUri;
+            _context.Update(user);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
